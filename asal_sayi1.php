f8be3b7b98046bbd82cccf68654b1d1f335f194d
SaraAkman <78727522+SaraAkman@users.noreply.github.com>
2021-02-13 20:44:36 +0300

    Create asal_sayi1.php

---
+++ asal_sayi1.php
@@ -0,0 +1,28 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PHP Asal Sayı Bulma-1</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+ 
+<body>
+ 
+<form action="asalsayi2.php" method="post">
+
+
+
+    <td>Bir Sayı Giriniz:</td>
+    <td><input name="sayi" type="text" /></td>
+
+  
+
+    <td>&nbsp;</td>
+    <td><input name="gonder" type="submit" value="Hesapla" /></td>
+
+ 
+</form>
+<br />
+ 
+</body>
+</html>
+
