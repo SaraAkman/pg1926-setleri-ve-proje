99c79c8207173544befa2dc924b2e61878006093
SaraAkman <78727522+SaraAkman@users.noreply.github.com>
2021-02-13 20:48:11 +0300

    Create para_ustu1.php

---
+++ para_ustu1.php
@@ -0,0 +1,29 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Para üstü hesabı</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+ 
+<body>
+ 
+<form action="para_ustu.php" method="post">
+
+
+
+    <td>Para üstünü giriniz =</td>
+    <td><input name="sayi" type="text" /></td>
+  
+
+    <td>&nbsp;</td>
+    <td><input name="gonder" type="submit" value="Hesapla" /></td>
+    <br><td> NOT = ondalıklı sayıları nokta ile yazınız</td></br>
+
+
+ 
+</form>
+<br />
+ 
+</body>
+</html>
+
