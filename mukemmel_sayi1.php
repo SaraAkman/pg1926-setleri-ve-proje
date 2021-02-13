c2ac5e20f33f81fed80c8a535003b09eabcab58b
SaraAkman <78727522+SaraAkman@users.noreply.github.com>
2021-02-13 20:49:45 +0300

    Create mukemmel_sayi1.php

---
+++ mukemmel_sayi1.php
@@ -0,0 +1,28 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Mükemmel sayı bulma</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+ 
+<body>
+ 
+<form action="mukemmel_sayi.php" method="post">
+
+
+
+    <td>Bir Sayı Giriniz:</td>
+    <td><input name="sayi" type="text" /></td>
+
+  
+
+    <td>&nbsp;</td>
+    <td><input name="gonder" type="submit" value="Hesapla" /></td>
+
+ 
+</form>
+<br />
+ 
+</body>
+</html>
+
